847108a831f034716708d7fbcacef0b34157f014
KSpade8 <hkayan08@gmail.com>
2026-01-20 15:33:16 -0500

    Fix ESLint error in fibRoute

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -4,8 +4,15 @@ import fibonacci from "./fib";
 
 export default (req: any, res: any) => {
   const { num } = req.params;
+  const parsedNum = parseInt(num);
+
+  if (isNaN(parsedNum) || parsedNum < 0) {
+    res.send(`fibonacci(${num}) is undefined`);
+    return;
+  }
 
   const fibN = fibonacci(parseInt(num));
+
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
