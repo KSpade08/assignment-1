03ac5af23cc61f2edbc56e587391aa7b4e03b3b3
KSpade8 <hkayan08@gmail.com>
2026-01-20 20:53:04 -0500

    Fix ESLint error in fibRoute any type

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,18 +1,11 @@
 // Endpoint for querying the fibonacci numbers
-
+import {Request, Response} from "express";
 import fibonacci from "./fib";
 
-export default (req: any, res: any) => {
+const fibRoute = (req: Request, res: Response): void => {
   const { num } = req.params;
-  const parsedNum = parseInt(num);
-
-  if (isNaN(parsedNum) || parsedNum < 0) {
-    res.send(`fibonacci(${num}) is undefined`);
-    return;
-  }
-
+  //const parsedNum = parseInt(num, 10)
   const fibN = fibonacci(parseInt(num));
-
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
